db7c1d5d5b2ce4c2addf090b2880184ef891f88d
Ahmed Safwat <149934007+ASHmd97@users.noreply.github.com>
2023-11-20 14:44:55 +0200

    Update To_Do_List.py

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,16 +1,5 @@
 # Simple To-Do List Manager
 
-'''
-- This Python program serves as a user-friendly to-do list manager, allowing users to easily organize and track their tasks.
-- Upon launching the program, users are greeted with a warm welcome message and presented with a menu offering various options.
-- Users can add new tasks to the list, mark existing tasks as complete, view all tasks with their completion status, or exit the program.
-- The program utilizes a list of tasks, each represented by a dictionary containing a task name and a completion status (True or False).
-- Each menu option is implemented as a function, and the main loop continuously prompts the user for input until they choose to exit.
-- Comments are included throughout the code to provide clear explanations, making it accessible for users and future developers alike.
-- Enjoy a straightforward and effective task management experience with this Simple To-Do List Manager!
-
-'''
-
 # Initialize a list of tasks with their completion status
 tasks = [
     {"task": "Quraan", "status": True},
